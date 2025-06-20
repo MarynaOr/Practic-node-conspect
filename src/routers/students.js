b0f9b193f7ddad982f435e8cace526e958dcde45
Maryna <89296066105marina@gmal.com>
2025-06-20 17:18:54 +0200

    orgController

--- src/routers/students.js
+++ src/routers/students.js
@@ -1,36 +1,17 @@
 // routers - express-роутери, які будуть використані в застосунку
 import { Router } from 'express';
+
 import {
-  getAllStudents,
-  getStudentId,
-} from '../services/students.js';
+  getStudentsByIdController,
+  getStudentsController,
+} from '../controllers/student.js';
 
 const router = Router();
 
-router.get(
-  '/students',
-  async (req, res, next) => {
-    const students = await getAllStudents();
-    res.status(200).json({
-      data: students,
-    });
-  },
-);
+router.get('/students', getStudentsController);
 router.get(
   '/students/:studentId',
-  async (req, res, next) => {
-    const { studentId } = req.params;
-    const student = await getStudentId(studentId);
-    if (!student) {
-      res
-        .status(404)
-        .json({ message: 'Student not found' });
-    }
-    res.status(200).json({
-      data: student,
-    });
-    return;
-  },
+  getStudentsByIdController,
 );
 
 export default router;
