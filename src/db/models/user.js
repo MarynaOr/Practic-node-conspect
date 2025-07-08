f563b0525d46c9edb7ea6d9771194a13a9730b60
Maryna <89296066105marina@gmal.com>
2025-07-08 19:49:27 +0200

    uath-hash

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -22,6 +22,12 @@ const userSchema = new Schema(
   },
 );
 
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
 export const usersCollection = model(
   'User',
   userSchema,
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,20 @@
+import createHttpError from 'http-errors';
 import { usersCollection } from '../db/models/user.js';
+import bcrypt from 'bcrypt';
 
 export const registerUser = async (payload) => {
-  return await usersCollection.create(payload);
+  const user = await usersCollection.findOne({
+    email: payload.email,
+  });
+  if (user)
+    throw createHttpError(409, 'Email in use');
+
+  const encryptedPassword = await bcrypt.hash(
+    payload.password,
+    10,
+  );
+  return await usersCollection.create({
+    payload,
+    password: encryptedPassword,
+  });
 };
